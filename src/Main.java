a9f871350dbf93de93d837e81bd716c26ec906ab
Sagher Mehmood <sagherhundal@gmail.com>
2016-06-29 21:18:30 +0500

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+package jnet;
+
+import java.io.IOException;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		new PacketInspector().pcapProcess();
+
+	}
+}
