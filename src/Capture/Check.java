bd4d88983d1b9a39cfdc84bcb374527b1d0dc2f4
Sagher Mehmood <sagherhundal@gmail.com>
2016-07-21 13:35:43 +0500

    Create Check.java

---
+++ src/Capture/Check.java
@@ -0,0 +1,48 @@
+package Capture;
+
+import Feeds.MainFeeds;
+
+public class Check {
+	public static  String isIPMalicious(String ip) {
+
+		
+		if(!ip.equals("115.186.132.18")){
+			
+			if (MainFeeds.Malwares.contains(ip)) {
+				return "MALWARES";
+			}
+			if (MainFeeds.DB.contains(ip)) {
+				return "DB";
+			}
+			if (MainFeeds.SIP.contains(ip)) {
+				return "SIP";
+			}
+			if (MainFeeds.SSH.contains(ip)) {
+				return "SSH";
+			}
+			if (MainFeeds.Web.contains(ip)) {
+				return "WEB";
+			}
+			if (MainFeeds.Probing.contains(ip)) {
+				return "PROBING";
+			}
+		 else {
+			return "none";
+		
+		 }
+				
+	}
+		return "NONE";
+		
+	}
+
+	public static boolean isUrlMalicious(String url) {
+		if (MainFeeds.Urls.contains(url)) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+}
