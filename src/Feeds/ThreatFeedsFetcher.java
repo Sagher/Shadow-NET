67cfb9d7629e54f22e4314fb978e3beaedeac0c4
Sagher Mehmood <sagherhundal@gmail.com>
2016-07-21 13:37:47 +0500

    Create ThreatFeedsFetcher.java

---
+++ src/Feeds/ThreatFeedsFetcher.java
@@ -0,0 +1,65 @@
+package Feeds;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import java.util.ArrayList;
+
+public class ThreatFeedsFetcher {
+
+	public  ArrayList<String> maliciousIPs = new ArrayList<String>();
+
+
+	public  ArrayList<String> FeedsFetchers(String url,String type) {
+
+		String IP_container;// space delimited ip list
+
+		try {
+
+			HttpClient client = new DefaultHttpClient();
+
+			HttpGet request = new HttpGet(url);
+
+			HttpResponse Response = client.execute(request);
+
+			HttpEntity entity = Response.getEntity();
+
+			System.out.println("----------------------------------------");
+			System.out.println(Response.getStatusLine());
+			System.out.println(type+"\n----------------------------------------");
+
+			BufferedReader breader;
+
+			try {
+
+				breader = new BufferedReader(new InputStreamReader((Response.getEntity().getContent())));
+				if (entity != null) {
+					while ((IP_container = breader.readLine()) != null) {
+
+						maliciousIPs.add(IP_container);
+
+					}
+
+					breader.close();
+
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+
+			}
+
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return maliciousIPs;
+	}
+
+}
