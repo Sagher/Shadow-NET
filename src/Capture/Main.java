2730688f911c82a63e0b792d667cd30b60d6ba14
Hafiz-ATHAR <h.m.athar1@hotmail.com>
2016-09-19 16:19:10 +0500

    Update Main.java

--- src/Capture/Main.java
+++ src/Capture/Main.java
@@ -1,43 +1,63 @@
-package Capture;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.jnetpcap.Pcap;
+import org.jnetpcap.PcapBpfProgram;
 import org.jnetpcap.PcapIf;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		List<PcapIf> alldevs = new ArrayList<PcapIf>(); // Will be filled with NICs
-		StringBuilder errbuf = new StringBuilder(); // For any error msgs
+	public static final Logger debugger = Logger.getLogger("debugLogger");
 
-		/***************************************************************************
-		 * First get a list of devices on this system
-		 **************************************************************************/
+	public static void main(String[] args) throws IOException {
+
+		List<PcapIf> alldevs = new ArrayList<PcapIf>();
+		StringBuilder errbuf = new StringBuilder();
+
 		int r = Pcap.findAllDevs(alldevs, errbuf);
 		if (r != Pcap.OK || alldevs.isEmpty()) {
-			System.err.printf("Can't read list of devices, error is %s", errbuf.toString());
+			debugger.debug("Can't read list of devices, error is " + errbuf.toString());
 			return;
 		}
-
-		System.out.println("Network devices found:");
 
 		int i = 0;
 		for (PcapIf device : alldevs) {
-
 			System.out.println((i++) + "\t" + device.getName());
 		}
+		
 
-		Pcap pcap = Pcap.openLive(alldevs.get(0).getName(), 64*1024, Pcap.MODE_PROMISCUOUS, 10*1000, errbuf);
+		PcapIf device = alldevs.get(4);
+		debugger.info(device.getName());
 
+		int snaplen = 64 * 1024;
+		int flags = Pcap.MODE_NON_PROMISCUOUS;
+		int timeout = 10 * 10000;
+		Pcap pcap = Pcap.openLive(device.getName(), snaplen, flags, timeout, errbuf);
+		
 		if (pcap == null) {
-			System.err.printf("Error while opening device for capture: " + errbuf.toString());
+			debugger.debug("Error while opening device for capture: " + errbuf.toString());
 			return;
 		}
-		pcap.loop(-1, new PacketInspector(), " ");
-
+		
+		String expression = "tcp port http";  
+		int optimize = 0;         // 0 = false  
+		int netmask = 0xFFFFFF00; // 255.255.255.0  
+        PcapBpfProgram program = new PcapBpfProgram();
+        if (pcap.compile(program, expression, optimize, netmask) != Pcap.OK) {  
+        	  debugger.debug(pcap.getErr());  
+        	  return;  
+        	}  
+       
+        	          
+        	if (pcap.setFilter(program) != Pcap.OK) {  
+        		debugger.debug(pcap.getErr());  
+        	  return;         
+        	}  
+	
+		pcap.loop(-1, new PacketInspector(), "");
 		pcap.close();
+
 	}
 }
